f8281f7ff5b604fda1776ebd4f8fd29626ccda60
kakoucedricakrou <cedric.akrou@cinetpay.com>
2022-12-22 10:12:34 +0000

    feat: :recycle:

--- src/adapter/secondary/networking/repositories/mock/user.repository-mock.ts
+++ src/adapter/secondary/networking/repositories/mock/user.repository-mock.ts
@@ -35,7 +35,7 @@ export class UserRepositoryMock implements UserRepository{
             firstname : "Akrou Cedric",
             lastname: "KAKOU",
             email: "cedricakroustudy@gmail.com",
-            phone: ["+2250709882886", "+22540746469"],
+            phones: ["+2250709882886", "+22540746469"],
             address: address,
             role: role,
             menus: new MenuFakeData().datas(),
